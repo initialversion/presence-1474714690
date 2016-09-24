bed1453b826aeccf164f65b4cf702ecd0a0043f4
Murugan <murugu2012@gmail.com>
2016-09-24 16:28:38 +0530

    Validate uniqueness of users.integer1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :integer1, :uniqueness => { :scope => [:last_name, :integer3], :allow_blank => true, :case_sensitive => true, :message => "unique message" }
+
   validates :integer1, :length => { :minimum => 30, :maximum => 50, :allow_blank => true }
 
   validates :integer2, :numericality => { :equal_to => 12, :less_than => 13, :other_than => 15, :greater_than => 10, :less_than_or_equal_to => 14, :greater_than_or_equal_to => 11, :even => true }
