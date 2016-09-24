3ddcf38946abea1f87d155df478a6b3cd0956d58
Murugan <murugu2012@gmail.com>
2016-09-24 16:28:38 +0530

    Validate numericality of users.integer2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :integer2, :numericality => { :equal_to => 12, :less_than => 13, :other_than => 15, :greater_than => 10, :less_than_or_equal_to => 14, :greater_than_or_equal_to => 11, :even => true }
+
   validates :last_name, :presence => true
 
   validates :name, :presence => { :message => "should be present" }
